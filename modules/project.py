d9137b08e8f3c029a769305095aae3c025fd20fc
이정현 <jeonghyun0126@outlook.com>
2019-10-14 22:45:56 +0900

    프로젝트 생성 기능 추가

--- modules/project.py
+++ modules/project.py
@@ -9,13 +9,12 @@ def project_blueprint(db):
     def create_user():
         user = get_gh_user_info()
 
-        print(get_gh_projects_info(user["login"]))
-
         if request.method == "GET":
             return render_template(
                 "project/submit_project.html",
                 username = user["login"],
-                projects = get_gh_projects_info(user["login"])
+                projects = get_gh_projects_info(user["login"]),
+                devstacks = db.devstacks.find()
             )
         elif request.method == "POST":
             db_user = db.users.find_one({
@@ -23,12 +22,16 @@ def project_blueprint(db):
             })
             db_user["project_rank"] = 0
 
-            db.projects.insert_one({
-                "name": request.form["name"],
-                "rank": 0,
-                "proj_stacks": request.form["proj_stacks"],
-                "collaborators": [db_user]
-            })
+            db.projects.replace_one(
+                {
+                    "name": request.form["name"]
+                }, {
+                    "name": request.form["name"],
+                    "rank": 0,
+                    "proj_stacks": request.form["proj_stacks"].split(" ")[1:],
+                    "collaborators": [db_user]
+                }
+            )
 
             return "Good"
 
