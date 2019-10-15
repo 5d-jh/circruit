b72216f75ded7c4c5ef207e503e3470ae6c7337d
이정현 <jeonghyun0126@outlook.com>
2019-10-15 15:32:49 +0900

    사용자 토큰을 파일에 저장

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from pymongo import MongoClient
 from modules.user import user_blueprint
 from modules.project import project_blueprint
 from modules.api_func import get_gh_user_info, auth_required
+from modules.token_storage import FileStorage
+
+mongo_client = MongoClient(os.environ["DATABASE_URL"])
+db = mongo_client.circruit
 
 app = Flask(__name__)
 app.secret_key = "NEA#@WREBFsdfb{"
@@ -12,13 +16,11 @@ blueprint = make_github_blueprint(
     client_id="28e9cff80df971929acf",
     client_secret="e334101fb61d08f4198cf521bb8103b715fd5043",
     redirect_url="/user/authorize",
-    scope="repo,user"
+    scope="repo,user",
+    storage=FileStorage("tokens/auth.json")
 )
 app.register_blueprint(blueprint, url_prefix="/login")
 
-mongo_client = MongoClient(os.environ["DATABASE_URL"])
-db = mongo_client.circruit
-    
 @app.route("/")
 @auth_required
 def index():
