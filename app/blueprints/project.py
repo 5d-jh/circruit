90aa2040fba4dbb767d04222a8f33d67103e1c6f
5d-jh <jeonghyun0126@outlook.com>
2019-10-30 21:02:06 +0900

    GitHub hook 연동 추가

--- app/blueprints/project.py
+++ app/blueprints/project.py
@@ -5,6 +5,7 @@ from flask import Blueprint, request, render_template, redirect, url_for
 from flask_dance.contrib.github import github
 from modules.api_func import get_gh_projects_info
 from modules import auth_required, db_required
+import json
 
 blueprint = Blueprint('project', __name__)
 
@@ -103,3 +104,45 @@ def join_project(user, db, gh_usrname, proj_name):
         return redirect(f"/project/{gh_usrname}/{proj_name}")
     except:
         return "프로젝트에 참여하는 과정에서 오류가 발생했습니다.", 503
+
+@blueprint.route("/<gh_usrname>/<proj_name>/hook", methods=["POST"])
+@db_required
+def manage_project_todo(db, gh_usrname, proj_name):
+    hook_payload = json.loads(request.data)
+    print(hook_payload)
+
+    if hook_payload["action"] == "opened":
+        #Issue가 생성되면 새로운 todo 생성
+        db.projects.update_one(
+            {
+                "name": proj_name,
+                "owner.username": gh_usrname
+            }, {
+                "$push": {
+                    "todos": {
+                        "is_closed": False,
+                        "vote": 0,
+                        "id": hook_payload["issue"]["id"],
+                        "title": hook_payload["issue"]["title"],
+                        "link": hook_payload["issue"]["url"],
+                        "assignees": hook_payload["issue"]["assignees"]
+                    }
+                }
+            }
+        )
+         
+    if hook_payload["action"] == "assigned":
+        #Issue에 새로운 사람이 배정받게 되면 assignees 업데이트
+        db.projects.update_one(
+            {
+                "name": proj_name,
+                "owner.username": gh_usrname,
+                "todos.id": hook_payload["issue"]["id"]
+            }, {
+                "$set": {
+                    "todos.$.assignees": hook_payload["issue"]["assignees"]
+                }
+            }
+        )
+
+    return "OK", 200
